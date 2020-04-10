117fba893e75e30a37262d73e0c0ac24873753c6
Jessica Wei <jessicawei9@gmail.com>
2020-04-09 19:15:36 -0700

    Setting up prebuilts

---
+++ Assets/Scripts/PrebuiltVeggie.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PrebuiltVeggie : MonoBehaviour
+{
+    // Use double foward slashes to comment out one line
+    /** Or two forward slashes with two astricks in the
+     *  middle to comment out a section! (Comments will not effect the code)
+     */
+
+
+}
